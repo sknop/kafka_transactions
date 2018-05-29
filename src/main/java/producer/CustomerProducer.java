6b73c057f72ad2e07bb48f314554cbe122a73e6a
Sven Erik Knop <seknop@gmail.com>
2018-05-29 16:33:39 +0100

    I am going to reset for testing

--- src/main/java/producer/CustomerProducer.java
+++ src/main/java/producer/CustomerProducer.java
@@ -2,20 +2,38 @@ package producer;
 
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import net.sourceforge.argparse4j.ArgumentParserBuilder;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.QueryableStoreType;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import schema.Customer;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
 
@@ -45,41 +63,60 @@ public class CustomerProducer {
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
         properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
-        KafkaProducer<Integer, Object> producer = new KafkaProducer<>(properties);
 
-        return producer;
+        return new KafkaProducer<>(properties);
+    }
+
+    private KafkaStreams createStreams(Topology topology) {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "customer-producer");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Specify default (de)serializers for record keys and for record values.
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+
+        return new KafkaStreams(topology, properties);
     }
 
     private void produce() {
         KafkaProducer<Integer, Object> producer = createProducer();
-        // KTable<Integer, Object> customers =
+        StreamsBuilder builder = new StreamsBuilder();
 
+//        final Map<String, String> serdeConfig =
+//                Collections.singletonMap(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+//        final Serde<Customer> customerSerde = new SpecificAvroSerde<>();
+//        customerSerde.configure(serdeConfig, false)
 
-        Thread mainThread = Thread.currentThread();
+        KTable<Integer, Customer> existingCustomers = builder
+                .table(customerTopic, Materialized.<Integer, Customer, KeyValueStore<Bytes, byte[]>>as("customer-store"));
+        KafkaStreams streams = createStreams(builder.build());
+        streams.start();
+
+        String storeName = existingCustomers.queryableStoreName();
+        ReadOnlyKeyValueStore<Integer, Customer> keyValueStore = streams.store(storeName, QueryableStoreTypes.keyValueStore());
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Shutting down gracefully ...");
-            mainThread.interrupt();
+            producer.close();
+            streams.close();
         }));
 
         while(true) {
-            ProducerRecord<Integer, Object> record = createRecord(producer);
+            ProducerRecord<Integer, Object> record = createRecord(keyValueStore);
 
             producer.send(record);
+            producer.flush();
 
             System.out.println("Produced " + record);
-
-            if(mainThread.isInterrupted()) {
-               producer.flush();
-               producer.close();
-
-               return;
-            }
 
         }
     }
 
-    private ProducerRecord<Integer,Object> createRecord(KafkaProducer<Integer,Object> producer) {
+    private ProducerRecord<Integer,Object> createRecord(ReadOnlyKeyValueStore<Integer, Customer> keyValueStore) {
         // create a random number up to max_customers
         // search db for that number
         // if found:
@@ -90,24 +127,37 @@ public class CustomerProducer {
         // save object
         // produce and return record0
 
-        int customerId = random.nextInt(maxCustomers) + 1;
-        String firstName = "first_" + customerId;
-        String lastName = "last_" + customerId;
-        String email = "email_" + customerId + "@email.com";
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss xxx");
         LocalDateTime now = LocalDateTime.now();
 
         String date = dtf.format(now);
-        int epoch = 1;
 
-        Customer customer = new Customer(customerId, firstName, lastName, email, date, epoch);
+        int customerId = random.nextInt(maxCustomers) + 1;
 
-        return new ProducerRecord<>(customerTopic, customerId, customer);
+        Customer probe = keyValueStore.get(customerId);
+        if (probe != null) {
+            probe.setEpoch(probe.getEpoch() + 1);
+            probe.setUpdate(date);
+
+            System.out.println("*** Found existing customer. Updating to " + probe);
+
+            return new ProducerRecord<>(customerTopic, probe.getCustomerId(), probe);
+        }
+        else {
+            String firstName = "first_" + customerId;
+            String lastName = "last_" + customerId;
+            String email = "email_" + customerId + "@email.com";
+
+            int epoch = 1;
+
+            Customer customer = new Customer(customerId, firstName, lastName, email, date, epoch);
+
+            return new ProducerRecord<>(customerTopic, customerId, customer);
+        }
     }
 
     public static void main(String[] args) {
-        ArgumentParserBuilder builder = ArgumentParsers.newFor("TransactionalProducer").addHelp(true);
+        ArgumentParserBuilder builder = ArgumentParsers.newFor("CustomerProducer").addHelp(true);
 
         ArgumentParser parser = builder.build();
         parser.addArgument("-m", "--max-customers")
@@ -140,6 +190,7 @@ public class CustomerProducer {
         }
         catch (Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
     }
 
