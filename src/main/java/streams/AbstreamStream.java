ffd1c19b379d35fc42a267307907d138c30a6d9e
Sven Erik Knop <seknop@gmail.com>
2020-06-29 13:22:15 +0100

    Add AbstractStream to standardize on CLI for stream applications

---
+++ src/main/java/streams/AbstreamStream.java
@@ -0,0 +1,84 @@
+package streams;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
+import picocli.CommandLine;
+
+import java.io.*;
+import java.util.Properties;
+
+@CommandLine.Command(
+        synopsisHeading = "%nUsage:%n",
+        descriptionHeading   = "%nDescription:%n%n",
+        parameterListHeading = "%nParameters:%n%n",
+        optionListHeading    = "%nOptions:%n%n",
+        mixinStandardHelpOptions = true,
+        sortOptions = false)
+public abstract class AbstreamStream {
+    @CommandLine.Option(names = {"--bootstrap-servers"},
+            description = "Bootstrap Servers (default = ${DEFAULT-VALUE})",
+            defaultValue = "localhost:9092")
+    protected String bootstrapServers;
+
+    @CommandLine.Option(names = {"--schema-registry"},
+            description = "Schema Registry (default = ${DEFAULT-VALUE})",
+            defaultValue = "http://localhost:8081")
+    protected String schemaRegistryURL;
+
+    @CommandLine.Option(names = {"-c", "--config-file"},
+            description = "If provided, content will be added to the properties")
+    protected String configFile = null;
+
+    @CommandLine.Option(names = {"-v", "--verbose"},
+            description = "If enabled, will print out every message created")
+    protected boolean verbose = false;
+
+    protected KafkaStreams createStreams(Topology topology) {
+        Properties properties = new Properties();
+
+        if (configFile != null) {
+            try (InputStream inputStream = new FileInputStream(configFile)) {
+                Reader reader = new InputStreamReader(inputStream);
+
+                properties.load(reader);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.err.println("Inputfile " + configFile + " not found");
+                System.exit(1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, getApplicationName());
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Specify default (de)serializers for record keys and for record values.
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+
+        // properties.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+        properties.put(
+                StreamsConfig.CONSUMER_PREFIX + ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,
+                "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor");
+        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
+        properties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
+                LogAndContinueExceptionHandler.class.getName());
+
+        return new KafkaStreams(topology, properties);
+    }
+
+    protected abstract String getApplicationName();
+
+}
--- src/main/java/streams/CustomerStream.java
+++ src/main/java/streams/CustomerStream.java
@@ -1,67 +1,31 @@
 package streams;
 
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import net.sourceforge.argparse4j.ArgumentParserBuilder;
-import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.impl.Arguments;
-import net.sourceforge.argparse4j.inf.ArgumentParser;
-import net.sourceforge.argparse4j.inf.ArgumentParserException;
-import net.sourceforge.argparse4j.inf.Namespace;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
 import org.apache.kafka.streams.kstream.KStream;
+import picocli.CommandLine;
 import schema.Customer;
 
-import java.util.Properties;
+import java.util.concurrent.Callable;
 
-public class CustomerStream {
+@CommandLine.Command(name = "CustomerStream",
+        version = "CustomerStream 1.0",
+        description = "Reads Customer objects in Avro format from a stream.")
+public class CustomerStream extends AbstreamStream implements Callable<Integer> {
     final static String CUSTOMER_TOPIC = "customer";
     final static String BOOTSTRAP_SERVERS = "localhost:9092";
     final static String SCHEMA_REGISTRY_URL = "http://localhost:8081";
 
-    private String customerTopic;
+    @CommandLine.Option(names = {"--topic"},
+            description = "Topic for the object (default = ${DEFAULT-VALUE})")
+    private String customerTopic = CUSTOMER_TOPIC;
 
-    private String bootstrapServers;
-    private String schemaRegistryURL;
-
-    private boolean verbose;
-
-    public CustomerStream(Namespace options) {
-        customerTopic = options.get("customer_topic");
-        bootstrapServers = options.get("bootstrap_servers");
-        schemaRegistryURL = options.get("schema_registry");
-        verbose = options.get("verbose");
+    public CustomerStream() {
     }
 
-    private KafkaStreams createStreams(Topology topology) {
-        Properties properties = new Properties();
-        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "customer-stream");
-        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        // Specify default (de)serializers for record keys and for record values.
-        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
-        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-
-        properties.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
-
-        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
-        properties.put(
-                StreamsConfig.CONSUMER_PREFIX + ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,
-                "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor");
-        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
-        properties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
-                LogAndContinueExceptionHandler.class.getName());
-
-        return new KafkaStreams(topology, properties);
+    @Override
+    protected String getApplicationName() {
+        return "customer-stream";
     }
 
     private void consume() {
@@ -88,40 +52,21 @@ public class CustomerStream {
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
+
+    @Override
+    public Integer call() {
+        consume();
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-        ArgumentParserBuilder builder = ArgumentParsers.newFor("CustomerProducer").addHelp(true);
-
-        ArgumentParser parser = builder.build();
-        parser.addArgument("--customer-topic")
-                .type(String.class)
-                .setDefault(CUSTOMER_TOPIC)
-                .help(String.format("Topic for the customer (default %s)", CUSTOMER_TOPIC));
-        parser.addArgument("--bootstrap-servers")
-                .type(String.class)
-                .setDefault(BOOTSTRAP_SERVERS)
-                .help(String.format("Kafka Bootstrap Servers(default %s)", BOOTSTRAP_SERVERS));
-        parser.addArgument("--schema-registry")
-                .type(String.class)
-                .setDefault(SCHEMA_REGISTRY_URL)
-                .help(String.format("Schema registry URL(de fault %s)", SCHEMA_REGISTRY_URL));
-        parser.addArgument("-v", "--verbose")
-                .action(Arguments.storeConst())
-                .setDefault(Boolean.FALSE)
-                .setConst(Boolean.TRUE)
-                .help("If enabled, will print out every message consumed");
-
         try {
-            Namespace options = parser.parseArgs(args);
-
-            CustomerStream cs = new CustomerStream(options);
-            cs.consume();
-
-        } catch (ArgumentParserException e) {
-            System.err.println(parser.formatHelp());
-        } catch(Exception e) {
+            new CommandLine(new CustomerStream()).execute(args);
+        }
+        catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
-
     }
 }
