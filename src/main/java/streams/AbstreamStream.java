6841e93bee55fee8eefede9a64f67f94a3681284
Sven Erik Knop <seknop@gmail.com>
2020-09-27 14:36:01 +0100

    Correct handling of config file and defaults.

--- src/main/java/streams/AbstreamStream.java
+++ src/main/java/streams/AbstreamStream.java
@@ -1,8 +1,11 @@
 package streams;
 
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
@@ -21,14 +24,15 @@ import java.util.Properties;
         mixinStandardHelpOptions = true,
         sortOptions = false)
 public abstract class AbstreamStream {
+    private static final String DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String DEFAULT_SCHEMA_REGISTRY =  "http://localhost:8081";
+
     @CommandLine.Option(names = {"--bootstrap-servers"},
-            description = "Bootstrap Servers (default = ${DEFAULT-VALUE})",
-            defaultValue = "localhost:9092")
+            description = "Bootstrap Servers (default = ${DEFAULT-VALUE})")
     protected String bootstrapServers;
 
     @CommandLine.Option(names = {"--schema-registry"},
-            description = "Schema Registry (default = ${DEFAULT-VALUE})",
-            defaultValue = "http://localhost:8081")
+            description = "Schema Registry (default = ${DEFAULT-VALUE})")
     protected String schemaRegistryURL;
 
     @CommandLine.Option(names = {"-c", "--config-file"},
@@ -39,8 +43,15 @@ public abstract class AbstreamStream {
             description = "If enabled, will print out every message created")
     protected boolean verbose = false;
 
+    @CommandLine.Option(names = {"--enable-monitoring-interceptor"},
+            description = "Enable MonitoringInterceptors (for Control Center)")
+    protected boolean monitoringInterceptors = false;
+
     protected KafkaStreams createStreams(Topology topology) {
         Properties properties = new Properties();
+
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, DEFAULT_BOOTSTRAP_SERVERS);
+        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, DEFAULT_SCHEMA_REGISTRY);
 
         if (configFile != null) {
             try (InputStream inputStream = new FileInputStream(configFile)) {
@@ -57,7 +68,12 @@ public abstract class AbstreamStream {
         }
 
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, getApplicationName());
-        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        if (bootstrapServers != null) {
+            properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        }
+        if (schemaRegistryURL != null) {
+            properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+        }
 
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
@@ -67,14 +83,16 @@ public abstract class AbstreamStream {
 
         // properties.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
 
-        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
-        properties.put(
-                StreamsConfig.CONSUMER_PREFIX + ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,
-                "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor");
-        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
-//        properties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
-//                LogAndContinueExceptionHandler.class.getName());
+        if (monitoringInterceptors) {
+            properties.put(
+                    StreamsConfig.CONSUMER_PREFIX + ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,
+                    "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor");
+            properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+            properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
+        }
+
+        //        properties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
+        //                LogAndContinueExceptionHandler.class.getName());
 
         addConsumerProperties(properties);
 
