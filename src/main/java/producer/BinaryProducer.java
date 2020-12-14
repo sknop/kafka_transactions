87b80b049651a40b0b906a64aa7ee3d3185e369b
Sven Erik Knop <seknop@gmail.com>
2020-12-14 17:25:21 +0000

    Test convert binary to hex string with streams app

---
+++ src/main/java/producer/BinaryProducer.java
@@ -0,0 +1,61 @@
+package producer;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import picocli.CommandLine;
+import schema.Binary;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "BinaryProducer",
+        version = "BinaryProducer 1.0",
+        description = "Produces Binary objects in Avro format, either a fixed amount or continuously.")
+public class BinaryProducer extends AbstractProducer implements Callable<Integer> {
+    @CommandLine.Option(names = {"--binary-topic"},
+            description = "Topic for the customer (default = ${DEFAULT-VALUE})",
+            defaultValue = "binary")
+    private String binaryTopic;
+    private BigInteger counter = new BigInteger("430E009A80B53D7896A57BFE4DD91954", 16);
+
+    public BinaryProducer() {  }
+
+
+    @Override
+    protected void addProducerProperties(Properties properties) {
+    }
+
+    @Override
+    protected ProducerRecord<Integer,Object> createRecord() {
+
+        counter = counter.add(BigInteger.valueOf(1));
+
+        var buffer = ByteBuffer.wrap(counter.toByteArray());
+        String hexKey = counter.toString(16);
+
+        Binary binary = new Binary(buffer, hexKey);
+
+        return new ProducerRecord<>(binaryTopic, counter.intValue(), binary);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        produce();
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            new CommandLine(new BinaryProducer()).execute(args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+}
+
---
+++ src/main/java/streams/BinaryConversionStream.java
@@ -0,0 +1,92 @@
+package streams;
+
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import picocli.CommandLine;
+import schema.Binary;
+import schema.HexString;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "BinaryConversionStream",
+        version = "BinaryConversionStream 1.0",
+        description = "Reads Binary objects in Avro format and converts binary fields to Hex.")
+public class BinaryConversionStream extends AbstreamStream implements Callable<Integer> {
+    final static String BINARY_TOPIC = "schema.Binary";
+    final static String HEXSTRING_TOPIC = "schema.HexString";
+
+    @CommandLine.Option(names = {"--binary-topic"},
+            description = "Topic for the Binary object (default = ${DEFAULT-VALUE})")
+    private String binaryTopic = BINARY_TOPIC;
+
+    @CommandLine.Option(names = {"--hexstring-topic"},
+            description = "Topic for the HexString object (default = ${DEFAULT-VALUE})")
+    private String hexStringTopic = HEXSTRING_TOPIC;
+
+    public BinaryConversionStream() {
+    }
+
+    @Override
+    protected String getApplicationName() {
+        return "binary-conversion-stream";
+    }
+
+    @Override
+    protected void addConsumerProperties(Properties properties) {
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+    }
+
+    private void consume() {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<Integer, Binary> existingBinary = builder.stream(binaryTopic);
+
+        if (verbose)
+            existingBinary.foreach((key, value) -> System.out.println(key + " => " + value));
+
+        existingBinary.mapValues(value -> convertFromBinary(value))
+                .peek((key, value) -> System.out.println(key + " => " + value))
+                .to(hexStringTopic);
+
+        KafkaStreams streams = createStreams(builder.build());
+        streams.setStateListener((newState, oldState) -> System.out.println("*** Changed state from " +oldState + " to " + newState));
+        streams.start();
+
+        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    private HexString convertFromBinary(Binary binary) {
+        ByteBuffer id = binary.getId();
+        BigInteger value = new BigInteger(id.array());
+
+        return new HexString(value.toString(16), binary.getPayload());
+    }
+
+    @Override
+    public Integer call() {
+        consume();
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        try {
+            new CommandLine(new BinaryConversionStream()).execute(args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
