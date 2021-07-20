ddf63304732bb1f1e7179ddbc4093bc4eb0b56dd
Sven Erik Knop <seknop@gmail.com>
2021-07-20 14:07:32 +0100

    Need to create Network first, then everything runs.

--- src/test/java/producer/CustomerProducerTest.java
+++ src/test/java/producer/CustomerProducerTest.java
@@ -1,13 +1,11 @@
 package producer;
 
-import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 import testcontainers.SchemaRegistryContainer;
 
@@ -16,17 +14,13 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CustomerProducerTest {
-    @RegisterExtension
-    @Order(1)
     public static Network network = Network.newNetwork();
 
-    @RegisterExtension
-    @Order(2)
+    @Container
     public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.0"))
             .withNetwork(network);
 
-    @RegisterExtension
-    @Order(3)
+    @Container
     public static SchemaRegistryContainer schemaRegistry = new SchemaRegistryContainer(DockerImageName.parse("confluentinc/cp-schema-registry:6.2.0"))
             .withKafka(kafka);
 
