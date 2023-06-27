3ee8dd913b49b1a9c2a5916d79be7ebe104ce688
Sven Erik Knop <seknop@gmail.com>
2023-06-27 09:00:03 +0100

    Start of a test case, not done yet.

---
+++ src/test/java/streams/CustomerDeduplicationStreamTest.java
@@ -0,0 +1,35 @@
+package streams;
+
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Properties;
+
+public class CustomerDeduplicationStreamTest {
+
+    protected TopologyTestDriver testDriver;
+    protected CustomerDeduplicateJoin streamApp = new CustomerDeduplicateJoin();
+    private static final String SCHEMA_REGISTRY_SCOPE = CustomerDeduplicationStreamTest.class.getName();
+    private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+
+    @BeforeEach
+    void setUp() {
+        var config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+
+        var builder = new StreamsBuilder();
+        streamApp.createTopology(builder);
+        var topology = builder.build();
+
+        testDriver = new TopologyTestDriver(topology, config);
+    }
+
+    @AfterEach
+    void tearDown() {
+        testDriver.close();
+    }
+}
